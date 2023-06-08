aff19679cddb277ef9b1d795ce23bc07b0b319c8
shubham jain <shubhamjain244122@gmail.com>
2023-06-09 01:29:57 +0530

    2componets

--- vervex/src/App.js
+++ vervex/src/App.js
@@ -1,21 +1,24 @@
 import HeroSection from "./Pages/Home/HeroSection";
+import Navbar from "./Components/Navbar";
+import AboutCompany from './Pages/Home/AboutCompany'
+import {
+  Route,
+  Switch,
+  BrowserRouter , Router,
+  Routes,
+} from "react-router-dom";
+import './app.css'
 
 function App() {
   return (
-    <div className="App">
-      <div>
-        <br />
-        <br />
-        "तत्त्वार्थसूत्र ज्ञानसम्पादनं, कर्मसम्यक्त्वाद् उत्तमं प्राप्ति।
-        अन्तःकरणशुद्धि प्रभवति, नैर्मल्येण देवस्य दर्शनम्॥"
-        <br />
-        hiii
-        <br />
-        "Commit to the Lord whatever you do, and your plans will succeed".
-        Proverbs 16:3
-      </div>
-      <HeroSection />
-    </div>
+    <BrowserRouter>
+    <Navbar/>
+    <AboutCompany/>
+    <Routes>
+      <Route path="/" >
+      </Route>
+    </Routes>
+  </BrowserRouter>
   );
 }
 
--- vervex/src/Pages/Home/AboutCompany.jsx
+++ vervex/src/Pages/Home/AboutCompany.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import '../../Styles/AboutCompany.css'
 
 const AboutCompany = () => {
   return (
-    <div>AboutCompany</div>
+    <>
+    <div className="AC_mainDiv">
+      
+      <div className="AC_headText">
+        A Software Development Company
+      </div>
+      <div className="AC_subText">
+      Vervex leverages advanced technologies like blockchain, web3, ML, and AI to deliver creative solutions. With a team of skilled IT Engineers and established processes, Vervex help businesses create bespoke software and applications for digital transformation.
+      </div>
+      <div className="AC_circle">
+      </div>
+    </div>
+    </>
   )
 }
 
