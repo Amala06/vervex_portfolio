06425651c7a0f022066a53e24cc587b0522fb9a7
Amalascrobits <90767720+Amala06@users.noreply.github.com>
2023-06-09 03:04:25 +0530

    created home an About Company first Revision

--- vervex/src/App.js
+++ vervex/src/App.js
@@ -1,20 +1,13 @@
+import AboutCompany from "./Pages/Home/AboutCompany";
 import HeroSection from "./Pages/Home/HeroSection";
 
+import "./Styles/App.css"
 function App() {
   return (
     <div className="App">
-      <div>
-        <br />
-        <br />
-        "तत्त्वार्थसूत्र ज्ञानसम्पादनं, कर्मसम्यक्त्वाद् उत्तमं प्राप्ति।
-        अन्तःकरणशुद्धि प्रभवति, नैर्मल्येण देवस्य दर्शनम्॥"
-        <br />
-        hiii
-        <br />
-        "Commit to the Lord whatever you do, and your plans will succeed".
-        Proverbs 16:3
-      </div>
       <HeroSection />
+      <AboutCompany />
+      {/* <Test/> */}
     </div>
   );
 }
