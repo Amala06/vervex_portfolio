825bb698b18b8a8f5640e4b3736321591aea91a0
shubham jain <shubhamjain244122@gmail.com>
2023-06-09 12:46:26 +0530

    Why us

--- vervex/src/App.js
+++ vervex/src/App.js
@@ -1,24 +1,20 @@
 import HeroSection from "./Pages/Home/HeroSection";
 import Navbar from "./Components/Navbar";
-import AboutCompany from './Pages/Home/AboutCompany'
-import {
-  Route,
-  Switch,
-  BrowserRouter , Router,
-  Routes,
-} from "react-router-dom";
-import './app.css'
+import AboutCompany from "./Pages/Home/AboutCompany";
+import { Route, Switch, BrowserRouter, Router, Routes } from "react-router-dom";
+import "./app.css";
+import WhyUs from "./Pages/Home/WhyUs";
 
 function App() {
   return (
     <BrowserRouter>
-    <Navbar/>
-    <AboutCompany/>
-    <Routes>
-      <Route path="/" >
-      </Route>
-    </Routes>
-  </BrowserRouter>
+      <Navbar />
+      <AboutCompany />
+      <WhyUs />
+      <Routes>
+        <Route path="/"></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
